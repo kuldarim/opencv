e3afb9ca0716cf15195033b7c25003aebc2b2cf5
Rimvydas Kulda <rimvydas.kulda@gmail.com>
2016-04-19 21:38:32 +0300

    open cv shows video camera correctly

---
+++ src/main/java/facedetection/MyFrame.java
@@ -0,0 +1,97 @@
+package facedetection;
+
+import org.opencv.core.*;
+import org.opencv.highgui.Highgui;
+import org.opencv.imgproc.Imgproc;
+import org.opencv.objdetect.CascadeClassifier;
+
+import java.awt.EventQueue;
+import java.awt.Graphics;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class MyFrame extends JFrame {
+    private JPanel contentPane;
+
+    CascadeClassifier faceDetector = new CascadeClassifier(
+            MyFrame
+                    .class
+                    .getResource("/haars/lbpcascade_frontalface.xml")
+                    .getPath()
+                    .substring(1));
+
+    /**
+     * Launch the application.
+     */
+    public static void main(String[] args) {
+        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                    MyFrame frame = new MyFrame();
+                    frame.setVisible(true);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    /**
+     * Create the frame.
+     */
+    public MyFrame() {
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setBounds(100, 100, 650, 490);
+        contentPane = new JPanel();
+        contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+        setContentPane(contentPane);
+        contentPane.setLayout(null);
+
+        new MyThread().start();
+    }
+
+    VideoCap videoCap = new VideoCap();
+
+    public void paint(Graphics g) {
+//        g = contentPane.getGraphics();
+//        g.drawImage(videoCap.getOneFrame(), 0, 0, this);
+//        Highgui.imwrite("src/main/resources/1.jpg", videoCap.getMat());
+//        MatOfRect faceDetections = new MatOfRect();
+//
+//        faceDetector.detectMultiScale(videoCap.getMat(), faceDetections);
+//
+//        for (Rect rect : faceDetections.toArray()) {
+//            System.out.println("ttt");
+//        }
+
+        g = contentPane.getGraphics();
+        g.drawImage(videoCap.getOneFrame(), 0, 0, this);
+
+        Mat mRgba = new Mat();
+        Mat mGrey = new Mat();
+        MatOfRect faces = new MatOfRect();
+        Mat input = videoCap.getMat();
+        input.copyTo(mRgba);
+        input.copyTo(mGrey);
+        Imgproc.cvtColor(mRgba, mGrey, Imgproc.COLOR_BGR2GRAY);
+        Imgproc.equalizeHist(mGrey, mGrey);
+        faceDetector.detectMultiScale(mGrey, faces);
+
+        for (Rect rect : faces.toArray()) {
+            System.out.println("ttt");
+        }
+    }
+
+    class MyThread extends Thread {
+        @Override
+        public void run() {
+            for (;;) {
+                repaint();
+                try { Thread.sleep(30);
+                } catch (InterruptedException e) {    }
+            }
+        }
+    }
+}
---
+++ src/main/java/facedetection/VideoCap.java
@@ -0,0 +1,28 @@
+package facedetection;
+
+import java.awt.image.BufferedImage;
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.highgui.VideoCapture;
+
+public class VideoCap {
+    static{
+        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+    }
+
+    VideoCapture cap;
+    Mat2Image mat2Img = new Mat2Image();
+
+    VideoCap(){
+        cap = new VideoCapture(0);
+    }
+
+    BufferedImage getOneFrame() {
+        cap.read(mat2Img.mat);
+        return mat2Img.getImage(mat2Img.mat);
+    }
+
+    Mat getMat() {
+        return mat2Img.mat;
+    }
+}
